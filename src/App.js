1afd7464257b574529a9cba95bd2be725f62381b
Sajjad <sajjadmehdi1214@gmail.com>
2021-05-18 12:44:28 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { isElementOfType } from 'react-dom/test-utils';
+import './App.css';
+
+function App() {
+  return (
+    <div className = "container listItembox">
+    <div className="row">
+      <section className="col-md">
+      <ul className="list-group">
+      <li className="list-group-item">
+            <h1>list item</h1>
+          </li>
+      
+    </ul>
+      </section >
+      <section className="col-md box" >
+        <div className="row">
+          <h1>list detail</h1>
+        </div>
+      </section>
+    </div>
+
+  </div>
+  );
+}
+
+export default App;
